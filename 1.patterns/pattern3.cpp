3b6e28bc3722bc0ac9dc9b182dd3a4477c14742e
SanketHajare44 <sankethajare44@gmail.com>
2026-08-20 23:11:03 +0530

    pattern printing

---
+++ 1.patterns/pattern3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class pattern{
+    public:
+        void PrintPattern(int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    cout<< j <<" ";
+                }
+
+                cout<<"\n";
+            }
+        }
+};
+
+int main(){
+
+    int iNo = 4;
+
+    pattern pt;
+
+    pt.PrintPattern(iNo);
+
+    return 0;
+}
