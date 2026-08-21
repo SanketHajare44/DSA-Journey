0e2cb634da0a0ee88e6cf0d5fb31a95269810b73
SanketHajare44 <sankethajare44@gmail.com>
2026-08-21 21:06:45 +0530

    Pattern Printing problelm

---
+++ 1.patterns/pattern18.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class pattern{
+    public:
+        void PrintPattern(int n)
+        {
+            
+            for(int i = 1; i <= n; i++)
+            {
+                char ch = 'A' + (n - i);
+
+                for(int j = 1; j<= i; j++)
+                {
+                    cout<< ch <<" ";
+                    ch++;      
+                }
+                cout<<"\n";
+            }
+        }
+};
+
+int main(){
+
+    int iNo = 4;
+
+    pattern pt;
+
+    pt.PrintPattern(iNo);
+
+    return 0;
+}
